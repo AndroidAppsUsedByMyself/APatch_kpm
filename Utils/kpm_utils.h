24a60b0880f87b64a8d445eaca1cda59a64e23e3
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-03-26 01:41:40 +0800

    chore: remove dup functions

--- Utils/kpm_utils.h
+++ Utils/kpm_utils.h
@@ -2,6 +2,7 @@
 #define _KPM_UTILS_H
 
 #include <asm/current.h>
+#include <taskext.h>
 #include <kpmodule.h>
 #include <kputils.h>
 #include <linux/cred.h>
@@ -9,27 +10,6 @@
 #include <linux/sched.h>
 
 #include "sk_func.h"
-
-#define TASK_EXT_MAGIC 0x1158115811581158
-
-struct task_ext
-{
-    // first
-    pid_t pid;
-    pid_t tgid;
-    int super;
-    int _;
-    int selinux_allow;
-    int priv_selinux_allow;
-    void *__;
-    // last
-    uint64_t magic;
-};
-
-static inline int task_ext_valid(struct task_ext *ext)
-{
-    return ext && (ext->magic == TASK_EXT_MAGIC);
-}
 
 static inline void get_current_ids(pid_t *pid, pid_t *tgid)
 {
@@ -40,21 +20,14 @@ static inline void get_current_ids(pid_t *pid, pid_t *tgid)
     }
 }
 
-static inline void set_priv_selinx_allow(int val)
-{
-    struct task_ext *ext = current_ext; //current_ext
-    if (likely(task_ext_valid(ext))) {
-        ext->priv_selinux_allow = val;
-        dsb(ish);
-    }
+static inline void set_priv_selinx_allow(struct task_struct* task, int val) {
+  struct task_ext* ext = get_task_ext(task);
+  if (likely(task_ext_valid(ext))) {
+    ext->priv_sel_allow = val;
+    dsb(ish);
+  }
 }
 
-static inline uid_t current_uid()
-{
-    struct cred *cred = *(struct cred **)((uintptr_t)current + task_struct_offset.cred_offset);
-    uid_t uid = *(uid_t *)((uintptr_t)cred + cred_offset.uid_offset);
-    return uid;
-}
 
 static inline void writeOutMsg(char *__user out_msg, int *outlen, const char *msg)
 {
